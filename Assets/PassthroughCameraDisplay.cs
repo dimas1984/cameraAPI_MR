fb6449fa676013a4f25bbccebe5cb3c581d81c14
DimasWW <dimas.w@polinema.ac.id>
2025-04-29 12:30:58 +0700

    change kode in passthroughCameraDisplay

--- Assets/PassthroughCameraDisplay.cs
+++ Assets/PassthroughCameraDisplay.cs
@@ -6,12 +6,13 @@ public class PassthroughCameraDisplay : MonoBehaviour
     public WebCamTextureManager WebCamTextureManager;
     public Renderer quadRenderer;
     public string textureName;
+    public float quadDistance = 1;
 
     private Texture2D m_texture;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        quadRenderer.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,12 +24,14 @@ public class PassthroughCameraDisplay : MonoBehaviour
             if(OVRInput.GetDown(OVRInput.Button.One) && WebCamTextureManager.WebCamTexture != null)
             {
                 TakePicture();
+                PlaceQuad();
             }
         }
     }
 
     public void TakePicture()
     {
+        quadRenderer.gameObject.SetActive(true);
         int widhth = WebCamTextureManager.WebCamTexture.width;
         int height = WebCamTextureManager.WebCamTexture.height;
 
@@ -44,6 +47,29 @@ public class PassthroughCameraDisplay : MonoBehaviour
 
         //quadRenderer.material.mainTexture = m_texture;
         quadRenderer.material.SetTexture(textureName, m_texture);
+    }
+
+    public void PlaceQuad()
+    {
+        Transform quadTransform = quadRenderer.transform;
+
+        Pose cameraPose = PassthroughCameraUtils.GetCameraPoseInWorld(PassthroughCameraEye.Left);
+        
+        Vector2Int  resolution = PassthroughCameraUtils.GetCameraIntrinsics(PassthroughCameraEye.Left).Resolution;
+
+        quadTransform.position = cameraPose.position + cameraPose.forward * quadDistance;
+        quadTransform.rotation = cameraPose.rotation;
+
+        Ray leftSide = PassthroughCameraUtils.ScreenPointToRayInCamera(PassthroughCameraEye.Left, new Vector2Int(0, resolution.y/2));
+        Ray rightSide = PassthroughCameraUtils.ScreenPointToRayInCamera(PassthroughCameraEye.Left, new Vector2Int(resolution.x, resolution.y / 2));
+
+
+        float horizontalFov =Vector3.Angle(leftSide.direction, rightSide.direction);
+        float quadScale = 2 * quadDistance*Mathf.Tan(horizontalFov * Mathf.Deg2Rad)/2;
+
+        float ratio = (float)m_texture.height / m_texture.width;
+
+        quadTransform.localScale = new Vector3(quadScale,quadScale*ratio,1);
     }
 
 }
